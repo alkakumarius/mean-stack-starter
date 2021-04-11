ef54b828b522a5348a8abc5a53f14837000e6559
Your Name <you@example.com>
2021-04-11 19:51:05 +0530

    review

--- client/src/modules/TodoServer/components/TodoUpdate.tsx
+++ client/src/modules/TodoServer/components/TodoUpdate.tsx
@@ -6,31 +6,31 @@ export default function TodoUpdate(props: IMasterUpdate) {
   const intialPost: Post = {
     email: "",
     comment: "",
-    type:"",
-    checked:true
+    type: "",
+    checked: true
   }
   const [post, setPost] = useState(intialPost);
   const [disabled, setDisabled] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     setPost(props.updatePost.post)
   }, [props])
 
   const isValid = (post: Post) => {
-    if(post.comment && post.comment.length === 0){
-        return false;
+    if (post.comment && post.comment.length === 0) {
+      return false;
     }
 
-    if(post.email && post.email.length === 0){
-        return false;
-    }
-    if(post.type && post.type.length === 0){
+    if (post.email && post.email.length === 0) {
       return false;
-  }
+    }
+    if (post.type && post.type.length === 0) {
+      return false;
+    }
 
     return true;
 
-}
+  }
 
   const handleOnChange = (e: { target: { name: string | number; value: any; }; }) => {
     let prevPost = JSON.parse(JSON.stringify(post));
@@ -38,24 +38,24 @@ export default function TodoUpdate(props: IMasterUpdate) {
     setPost(prevPost);
     const valid = isValid(prevPost);
 
-    if(valid === true){
+    if (valid === true) {
       setDisabled(false);
-     }else{
+    } else {
       setDisabled(true);
-     }
+    }
   }
   const handleOnChangeChecked = (e: { target: { name: string | number; checked: any; }; }) => {
     let prevPost = JSON.parse(JSON.stringify(post));
     prevPost[e.target.name] = e.target.checked;
     setPost(prevPost);
-  
+
     const valid = isValid(prevPost);
-   if(valid === true){
-    setDisabled(false);
-   }else{
-    setDisabled(true);
-   }
-}
+    if (valid === true) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }
 
 
   const handleClick = () => {
@@ -65,7 +65,7 @@ export default function TodoUpdate(props: IMasterUpdate) {
   return (
     <div className="container">
       <h1>Update Post</h1>
-      <hr/>
+      <hr />
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
         <input name="email" onChange={handleOnChange} value={post.email} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
@@ -75,22 +75,21 @@ export default function TodoUpdate(props: IMasterUpdate) {
         <textarea name="comment" onChange={handleOnChange} value={post.comment} className="form-control" id="exampleFormControlTextarea1" rows={3} ></textarea>
       </div>
       <div className="mb-3">
-                <select className="form-select" aria-label="Default select example"
-                    name="type" onChange={handleOnChange} value={post.type} >
-                    <option selected>Type</option>
-                    <option value="General Topic">General Topic</option>
-                    <option value="Computer Technology">Computer Technology</option>
-                    <option value="Science Technology"> Science Technology</option>
-                </select>
-            </div>
-            <div className="form-check">
-                <input className="form-check-input" type="checkbox"
-                    checked={post.checked} id="flexCheckChecked" name="checked" onChange={handleOnChangeChecked} />
-                <label className="form-check-label" htmlFor="flexCheckChecked">
-                    Checked checkbox
-                </label>
-            </div>
-      <button disabled={disabled} onClick={handleClick} type="button" className="btn btn-primary">Update Post</button>
+        <select className="form-select" aria-label="Default select example"
+          name="type" onChange={handleOnChange} value={post.type} >
+          <option selected>Type</option>
+          <option value="General Topic">General Topic</option>
+          <option value="Computer Technology">Computer Technology</option>
+          <option value="Science Technology"> Science Technology</option>
+        </select>
+      </div>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox"
+          checked={post.checked} id="flexCheckChecked" name="checked" onChange={handleOnChangeChecked} />
+        <label className="form-check-label" htmlFor="flexCheckChecked">
+          Available For Review
+        </label> 
+          <button disabled={disabled} onClick={handleClick} type="button" className="btn btn-primary">Update Post</button>
     </div>
   );
 }
